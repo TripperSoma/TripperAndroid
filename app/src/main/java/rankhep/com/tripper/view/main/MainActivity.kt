2880e68f061e0401d07589fd0a7079fdb293536f
nonop1507 <nono1507@naver.com>
2018-07-30 20:41:29 +0900

    Navigation bar tutorial

--- app/src/main/java/rankhep/com/tripper/view/main/MainActivity.kt
+++ app/src/main/java/rankhep/com/tripper/view/main/MainActivity.kt
@@ -7,6 +7,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 import rankhep.com.tripper.R
 import rankhep.com.tripper.view.main.presenter.MainContract
 import rankhep.com.tripper.view.main.presenter.MainPresenter
+import android.support.v4.view.GravityCompat
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import android.support.design.widget.NavigationView
+
 
 class MainActivity : AppCompatActivity(), MainContract.View {
     lateinit var mPresenter: MainPresenter
@@ -18,13 +24,56 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         mPresenter = MainPresenter()
         mPresenter.setView(this@MainActivity)
 
-        supportActionBar?.title = "Tripper"
-
+        setSupportActionBar(toolbar)
+        supportActionBar?.run{
+            title = "Tripper"
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+            setDisplayHomeAsUpEnabled(true)
+        }
         var adapter = MainReviewAdapter()
 
         reviewList.layoutManager = GridLayoutManager(this, 2)
         reviewList.adapter = adapter
+
+        navigation_view.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            drawer_layout.closeDrawers()
+
+            val id = menuItem.itemId
+            when (id) {
+                R.id.nav_item_home -> Toast.makeText(this@MainActivity, menuItem.title, Toast.LENGTH_LONG).show()
+
+                R.id.nav_item_airplane_reservation -> Toast.makeText(this@MainActivity, menuItem.title, Toast.LENGTH_LONG).show()
+
+                R.id.nav_item_hotel_reservation -> Toast.makeText(this@MainActivity, menuItem.title, Toast.LENGTH_LONG).show()
+
+                R.id.nav_item_my_daily -> Toast.makeText(this@MainActivity, menuItem.title, Toast.LENGTH_LONG).show()
+            }
+
+            true
+        }
     }
 
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+        when (id) {
+            android.R.id.home -> {
+                drawer_layout.openDrawer(GravityCompat.START)
+                return true
+            }
+            R.id.action_settings -> return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
 }
