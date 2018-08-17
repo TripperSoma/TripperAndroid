5fa87c14ba28b01504f7f75391ae724d29cd8357
nonop1507 <nono1507@naver.com>
2018-08-17 22:27:57 +0900

    Taste actviity

--- app/src/main/java/rankhep/com/tripper/view/add/set_taste/SetTasteActivity.kt
+++ app/src/main/java/rankhep/com/tripper/view/add/set_taste/SetTasteActivity.kt
@@ -1,7 +1,9 @@
 package rankhep.com.tripper.view.add.set_taste
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_set_taste.*
 import rankhep.com.tripper.R
 
 class SetTasteActivity : AppCompatActivity() {
@@ -9,5 +11,19 @@ class SetTasteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_set_taste)
+        initView()
+    }
+
+    private fun initView() {
+        setSupportActionBar(set_taste_toolbar)
+        supportActionBar?.run {
+            title = ""
+            setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp)
+            setDisplayHomeAsUpEnabled(true)
+        }
+        next_fab.setOnClickListener {
+            startActivity(Intent(this@SetTasteActivity, SetTasteActivity::class.java))
+        }
+
     }
 }
